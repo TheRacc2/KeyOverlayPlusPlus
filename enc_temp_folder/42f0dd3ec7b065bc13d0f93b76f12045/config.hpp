38ce17d4cca73099c34d33f2ee723e9bda8991ce
Liam James <liam.james@ymail.com>
2022-06-11 13:22:05 -0500

    refactor

---
+++ enc_temp_folder/42f0dd3ec7b065bc13d0f93b76f12045/config.hpp
@@ -0,0 +1,13 @@
+#ifndef CORE_CONFIG_HPP
+#define CORE_CONFIG_HPP
+
+namespace config {
+	namespace gui {
+		int keySpacing = 10;
+		int outlineColor = 0xFFFFFFFF;
+		int fillColor = 0xB9B4B4B4;
+		bool forceUppercase = true;
+	}
+}
+
+#endif
